2a83f608a7d5db42fb2d711807d2526476814eb9
thomasdang1809 <thomasdang1809@gmail.com>
2025-05-05 18:14:32 +0700

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -4,17 +4,14 @@ import './assets/style.css'
 import { BrowserRouter } from 'react-router-dom'
 import AppRouters from './AppRouters.tsx'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-import { ThemeProvider } from './components/theme/ThemeProvider.tsx'
-import { darkTheme } from './components/theme/theme.ts'
+
 
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <ThemeProvider theme={darkTheme}>
-          <AppRouters />
-        </ThemeProvider>
+        <AppRouters />
 
       </QueryClientProvider>
     </BrowserRouter>
